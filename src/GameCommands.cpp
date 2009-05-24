36d262ca2dac4673daace7c86363810329f9d854
FloSoft <FloSoft@siedler25.org>
2009-05-24 17:46:12 +0200

    committing subversion revision 4938 by FloSoft so lobby register sollte wieder funzen

--- src/GameCommands.cpp
+++ src/GameCommands.cpp
@@ -1,3 +1,25 @@
+// $Id: LobbyProtocol.h 4936 2009-05-24 15:17:07Z FloSoft $
+//
+// Copyright (c) 2005-2009 Settlers Freaks (sf-team at siedler25.org)
+//
+// This file is part of Siedler II.5 RTTR.
+//
+// Siedler II.5 RTTR is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// Siedler II.5 RTTR is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Siedler II.5 RTTR. If not, see <http://www.gnu.org/licenses/>.
+
+///////////////////////////////////////////////////////////////////////////////
+// Header
+#include "main.h"
 #include "GameCommands.h"
 #include "GameClientPlayer.h"
 #include "GameWorld.h"

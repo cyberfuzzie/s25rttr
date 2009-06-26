90ec52c6583b7b86afc65b2f9f8a357a3365a69f
FloSoft <FloSoft@siedler25.org>
2009-06-26 10:23:59 +0200

    committing subversion revision 5113 by FloSoft noch weng was aufgeräumt

--- dailyversion.sh
+++
@@ -1,6 +0,0 @@
-#/bin/sh
-
-if grep -q '^\s*#define WINDOW_VERSION "[0-9]\+"' version.h ; then
-	DATE=$(date +%Y%m%d)
-	sed -i -e "s~\(^\s*#define WINDOW_VERSION \"\)[0-9]\+\"~\1$DATE\"~" version.h
-fi

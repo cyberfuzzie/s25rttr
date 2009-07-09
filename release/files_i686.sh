84880451da2245465bcb46ec2abab117494ae110
FloSoft <FloSoft@siedler25.org>
2009-07-09 17:11:47 +0200

    committing subversion revision 5230 by FloSoft erstelle deb für repository-zeugs eingebaut

--- release/files_i686.sh
+++ release/files_i686.sh
@@ -10,7 +10,44 @@ cp -v ../../$BUILDDIR/driver/audio/*.so share/s25rttr/driver/audio
 cp -v ../../$BUILDDIR/s25update/src/s25update share/s25rttr/RTTR
 cp -v ../../$BUILDDIR/s-c/src/sound-convert share/s25rttr/RTTR
 
+
+
+# make deb-package
+PKGARCH=i386
+
+rm -rvf ../deb/s25rttr_$VERSION
+mkdir -vp ../deb/s25rttr_$VERSION/usr/share/{pixmaps,applications}
+mkdir -vp ../deb/s25rttr_$VERSION/DEBIAN
+
+cp -rv . ../deb/s25rttr_$VERSION/usr
+cp -rv ../../debian/control ../deb/s25rttr_$VERSION/DEBIAN/control
+
+# updater entfernen
+rm -vf ../deb/s25rttr_$VERSION/usr/share/s25rttr/RTTR/s25update
+
+cp -v ../../debian/s25rttr.png ../deb/s25rttr_$VERSION/usr/share/pixmaps/s25rttr.png
+cp -v ../../debian/s25rttr.desktop ../deb/s25rttr_$VERSION/usr/share/applications/s25rttr.desktop
+
+mkdir -vp ../deb/s25rttr_$VERSION/usr/share/doc/s25rttr
+mv -v ../deb/s25rttr_$VERSION/usr/*.txt ../deb/s25rttr_$VERSION/usr/share/doc/s25rttr
+
+sed -i "s/Installed-Size: x/Installed-Size: $(du -sk ../deb/s25rttr_$VERSION | cut -f 1)/" ../deb/s25rttr_$VERSION/DEBIAN/control
+sed -i "s/Version: x/Version: $(grep WINDOW_VERSION ../../../version.h | cut -d ' ' -f 3 | cut -d \" -f 2)-$(grep WINDOW_REVISION ../../../version.h | cut -d ' ' -f 3 | cut -d \" -f 2)/"  ../deb/s25rttr_$VERSION/DEBIAN/control
+sed -i "s/Architecture: x/Architecture: $PKGARCH/" ../deb/s25rttr_$VERSION/DEBIAN/control
+
+
+mkdir -vp ../../deb
+
+chown -R root.games ../deb/s25rttr_$VERSION
+dpkg -b ../deb/s25rttr_$VERSION/ ../../deb/s25rttr_${VERSION}_${PKGARCH}.deb
+dpkg-sig -v -k 6D09334C -s builder ../../deb/s25rttr_${VERSION}_${PKGARCH}.deb
+
+reprepro -Vb $TARGET/s25client includedeb unstable
+
+# copy to target
 if [ ! -z "$TARGET" ] ; then
+	reprepro -Vb $TARGET/s25client includedeb unstable ../../deb/s25rttr_${VERSION}_${PKGARCH}.deb
+	
 	mkdir -p $TARGET/$ARCH
 	
 	BINARIES="bin/rttr.sh share/s25rttr/RTTR/s25update bin/s25client share/s25rttr/RTTR/sound-convert"

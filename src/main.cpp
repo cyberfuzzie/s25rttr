b89fa7293a6d7a7c1a50dffcbf552a2d3c4bc223
FloSoft <FloSoft@siedler25.org>
2009-07-12 09:07:59 +0200

    committing subversion revision 5250 by FloSoft fehler korrigiert das die override-ordner nicht angelegt werden

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-// $Id: main.cpp 5247 2009-07-11 19:13:17Z FloSoft $
+// $Id: main.cpp 5250 2009-07-12 07:07:50Z FloSoft $
 //
 // Copyright (c) 2005-2009 Settlers Freaks (sf-team at siedler25.org)
 //
@@ -118,7 +118,7 @@ int main(int argc, char *argv[])
 
 	// diverse dirs anlegen
 	const unsigned int dir_count = 8;
-	unsigned int dirs[dir_count] = { 94, 47, 48, 51, 85, 95, 96, 97 }; // settingsdir muss zuerst angelegt werden (94)
+	unsigned int dirs[dir_count] = { 94, 47, 48, 51, 85, 98, 99, 100 }; // settingsdir muss zuerst angelegt werden (94)
 
 	for(unsigned int i = 0; i < dir_count; ++i)
 	{

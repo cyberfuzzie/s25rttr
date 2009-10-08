769699982caf120bb6afc7bf6d0122d1d5ceb9db
FloSoft <FloSoft@siedler25.org>
2009-10-08 19:07:21 +0200

    committing subversion revision 5613 by FloSoft so fehler korrigiert. woher das auch immer herkam. denk mal cmake hat da "was gepatcht" weils wohl mehr in den mailinglisten seit 2 tagen rundgeht bzgl dem fehler -.-

--- cmake/c.windows.i686.cmake
+++ cmake/c.windows.i686.cmake
@@ -6,10 +6,10 @@ SET(CMAKE_SYSTEM_PROCESSOR i686)
 SET(CMAKE_C_COMPILER   i586-mingw32msvc-gcc)
 SET(CMAKE_CXX_COMPILER i586-mingw32msvc-g++)
 SET(CMAKE_RANLIB i586-mingw32msvc-ranlib)
-SET(CMAKE_AR i586-mingw32msvc-ar)
 
-# set search prefix
-SET(CMAKE_PREFIX_PATH "/usr/i586-mingw32msvc")
+# find AR program
+FIND_PROGRAM(WINDOWS_AR NAMES i586-mingw32msvc-ar i686-mingw32-ar ar DOC "path to mingw's ar executable")
+SET(CMAKE_AR "${WINDOWS_AR}" CACHE PATH "the AR" FORCE)
 
 # where is the target environment 
 SET(CMAKE_FIND_ROOT_PATH "/usr/i586-mingw32msvc")

6f54e4da586035e08e696543301f4d004d007364
FloSoft <FloSoft@siedler25.org>
2010-03-25 13:27:21 +0100

    committing subversion revision 6196 by FloSoft grrr -.-

--- cmake/apple.common.cmake
+++ cmake/apple.common.cmake
@@ -1,5 +1,5 @@
 ################################################################################
-### $Id: apple.common.cmake 6195 2010-03-25 11:43:13Z FloSoft $
+### $Id: apple.common.cmake 6196 2010-03-25 12:27:15Z FloSoft $
 ################################################################################
 
 IF ( "${CMAKE_OSX_DEPLOYMENT_TARGET}" STREQUAL "" )
@@ -62,8 +62,8 @@ ENDIF ( NOT "${COMPILEARCH}" STREQUAL "" )
 # set SDK
 #IF ( "${CMAKE_PREFIX_PATH}" STREQUAL "" )
 #	SET(CMAKE_PREFIX_PATH "/Developer/SDKs/MacOSX10.4u.sdk")
-	FORCE_ADD_FLAGS(CMAKE_C_FLAGS -isysroot ${CMAKE_PREFIX_PATH})
-	FORCE_ADD_FLAGS(CMAKE_CXX_FLAGS -isysroot ${CMAKE_PREFIX_PATH})
+#	FORCE_ADD_FLAGS(CMAKE_C_FLAGS -isysroot ${CMAKE_PREFIX_PATH})
+#	FORCE_ADD_FLAGS(CMAKE_CXX_FLAGS -isysroot ${CMAKE_PREFIX_PATH})
 #ENDIF ( "${CMAKE_PREFIX_PATH}" STREQUAL "" )
 
 # search for programs in the build host directories

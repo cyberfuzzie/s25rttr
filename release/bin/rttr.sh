75a18a74452adcc5169adeb3e2b1bea21e149bf6
FloSoft <FloSoft@siedler25.org>
2010-07-03 11:59:44 +0200

    committing subversion revision 6536 by FloSoft startscript nochmal angepasst

--- release/bin/rttr.sh
+++ release/bin/rttr.sh
@@ -10,7 +10,25 @@ else
 	export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$DIR/../lib"
 fi
 
-if [ ! "$1" = "noupdate" ] ; then
+cmd=
+noupdate=0
+for I in $1; do
+	case I in
+		debug)
+			noupdate=1
+			cmd=gdb
+		;;
+		noupdate)
+			noupdate=1
+		;;
+		gdb|ldd|valgrind)
+			noupdate=1
+			cmd=$I
+		;;
+	esac
+done
+
+if [ noupdate -eq 0 ] ; then
 	if [ -f $DIR/../share/s25rttr/RTTR/s25update ] ; then
 		echo "checking for an update ..."
 		cp $DIR/../share/s25rttr/RTTR/s25update /tmp/s25update.$$
@@ -27,21 +45,6 @@ else
 fi
 
 chmod 0755 $DIR/../bin/rttr.sh $DIR/../share/s25rttr/RTTR/s25update $DIR/../bin/s25client $DIR/../share/s25rttr/RTTR/sound-convert >/dev/null 2>/dev/null
-
-CMD=
-
-case "$1" in
-	debug)
-		CMD=gdb
-	;;
-	valgrind)
-		CMD=valgrind
-	;;
-	ldd)
-		CMD=ldd
-	;;
-esac
-shift
 
 $CMD $DIR/../bin/s25client $*
 

8aedbdb8c5ff2defcb80ffb21fe9ca1d58e4a292
jh <jh@siedler25.org>
2010-07-18 12:46:10 +0200

    committing subversion revision 6589 by jh newline

--- src/Ware.cpp
+++ src/Ware.cpp
@@ -1,4 +1,4 @@
-// $Id: Ware.cpp 6582 2010-07-16 11:23:35Z FloSoft $
+// $Id: Ware.cpp 6589 2010-07-18 10:45:59Z jh $
 //
 // Copyright (c) 2005 - 2010 Settlers Freaks (sf-team at siedler25.org)
 //

32207bba085ca26a832be0188c6c0418fd7872ab
Sascha Riemer <sascha.riemer@gmail.com>
2013-03-01 21:56:31 +0100

    Add support for profiling build
    Add an option to configure for compiling a profiling build and set C and
    C++ compiler flags for this build

--- cmake/linux.common.cmake
+++ cmake/linux.common.cmake
@@ -6,6 +6,9 @@
 FORCE_ADD_FLAGS(CMAKE_C_FLAGS -ffast-math -mmmx -msse -mfpmath=sse -ggdb)
 FORCE_ADD_FLAGS(CMAKE_CXX_FLAGS -ffast-math -mmmx -msse -mfpmath=sse -ggdb)
 
+ADD_FLAGS(CMAKE_C_FLAGS_PROFILING -fno-inline -O0)
+ADD_FLAGS(CMAKE_CXX_FLAGS_PROFILING -fno-inline -O0)
+
 # search for programs in the build host directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 
